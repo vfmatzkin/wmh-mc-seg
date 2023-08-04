RegularizatorsFix + MC dropout + CompareRuns (Plot) - Fixed MEEP calculation (it was wrong) - Added KL-based regularizator - Added MC dropout - Changed some plots (for better comparison and not having the same notebook copied several times). Now all of them will be in notebooks/metrics.ipynb - Added top 1 checkpoint (same as top 3 but in the parent folder and always with the same name). - Updated README.md

src/losses/MEEP.py
@@ -2,20 +2,44 @@ import torch
 import torch.nn.functional as F
 
 
-class MEEP:
+class Regularizers:
+    """ MEEP/KL divergence term for the loss function
+
+    This therm is used as a regularizer for the loss function. It is used to
+    penalize the model for being too confident in its predictions in the
+    erroneous regions.
+
+    Note that this term has to be
+
+    """
+
+    def __init__(self, epsilon=1e-7, type='MEEP'):
+        self.epsilon = torch.tensor(epsilon).cuda() if torch.cuda.is_available() else torch.tensor(epsilon)
+        self.type = type
+
     def __call__(self, y_pred, y_true):
         y_pred_lm = torch.argmax(y_pred, dim=1)
         y_true_lm = torch.argmax(y_true, dim=1)
         misclassified_pixels = torch.not_equal(y_pred_lm, y_true_lm).float()
-        entropy = torch.mean(
-            F.binary_cross_entropy(y_pred, y_true, reduction="none"),
-            dim=1) * misclassified_pixels
-        entropy = torch.sum(entropy) / torch.sum(misclassified_pixels)
-        return entropy
+
+        # Clamp the predictions to prevent extreme values
+        y_pred_clamped = torch.clamp(y_pred, self.epsilon, 1.0 - self.epsilon)
+
+        if self.type == 'MEEP':
+            reg = torch.mean(
+                F.binary_cross_entropy(y_pred_clamped, y_pred_clamped, reduction="none"),
+                dim=1) * misclassified_pixels
+            reg = torch.sum(reg) / torch.sum(misclassified_pixels)
+        elif self.type == 'KL':
+            reg = torch.mean(
+                torch.log(y_pred_clamped),
+                dim=1) * misclassified_pixels
+            reg = torch.sum(reg) / torch.sum(misclassified_pixels)
+        return reg
 
 
 class BCEMEEPLoss(torch.nn.Module):
-    def __init__(self, start_on_epoch=0, meep_lambda=0.3):
+    def __init__(self, start_on_epoch=0, reg_lambda=0.3):
         """ Cross Entropy + MEEP Loss
 
         This loss function is a combination of the Binary Cross Entropy and the
@@ -26,20 +50,45 @@ class BCEMEEPLoss(torch.nn.Module):
         """
         super().__init__()
         self.CE = torch.nn.CrossEntropyLoss()
-        self.MEEP = MEEP()
-        self.m_lambda = meep_lambda
+        self.MEEP = Regularizers(type='MEEP')
+        self.m_lambda = reg_lambda
         self.start_on_epoch = start_on_epoch
 
     def forward(self, y_pred, y_true, epoch):
         use_meep = epoch >= self.start_on_epoch
 
-        print(f'Using MEEP: {use_meep}'
-              f' (epoch: {epoch}, start_on_epoch: {self.start_on_epoch})'
-              f' (m_lambda: {self.m_lambda})')
+        # print(f'Using MEEP: {use_meep}'
+        #       f' (epoch: {epoch}, start_on_epoch: {self.start_on_epoch})'
+        #       f' (m_lambda: {self.m_lambda})')
 
         ce = self.CE(y_pred, y_true)
         meep = self.MEEP(y_pred, y_true) if use_meep else 0
 
-        print(f'CE: {ce}, MEEP: {meep}')
+        # print(f'CE: {ce}, MEEP: {meep}')
 
-        return ce + self.m_lambda * meep
+        return {'ce': ce, 'meep': -self.m_lambda * meep}
+
+
+class BCEKLLoss(torch.nn.Module):
+    def __init__(self, start_on_epoch=0, reg_lambda=0.3):
+        """ Cross Entropy + KL Loss
+
+        This loss function is a combination of the Binary Cross Entropy and the
+        Maximum Entropy on Erroneous Predictions (MEEP) loss term.
+
+        In the paper, meep_lambda is set to 0.3 for Cross Entropy.
+        https://arxiv.org/pdf/2112.12218.pdf
+        """
+        super().__init__()
+        self.CE = torch.nn.CrossEntropyLoss()
+        self.KL = Regularizers(type='KL')
+        self.m_lambda = reg_lambda
+        self.start_on_epoch = start_on_epoch
+
+    def forward(self, y_pred, y_true, epoch):
+        use_kl = epoch >= self.start_on_epoch
+
+        ce = self.CE(y_pred, y_true)
+        kl = self.KL(y_pred, y_true) if use_kl else 0
+
+        return {'ce': ce, 'kl': -self.m_lambda * kl}

src/model.py
@@ -12,7 +12,7 @@ from monai.losses import DiceLoss, DiceCELoss, FocalLoss
 from monai.metrics import compute_dice as dice
 from torch.nn import CrossEntropyLoss
 
-from losses import BCEMEEPLoss
+from losses import BCEMEEPLoss, BCEKLLoss
 
 
 def compute_metrics(y_hat, y, text=''):
@@ -106,13 +106,13 @@ class WMHModel(pl.LightningModule):
 
     def __init__(self, net, criterion, learning_rate, optimizer_class,
                  weight_decay=0, lambda_lr=None, reduce_on_epoch=None,
-                 meep_start=0, meep_lambda=0.3, **kwargs):
+                 reg_start=0, reg_lambda=0.3, **kwargs):
         super().__init__()
 
         self.lr = learning_rate
         self.net = net
         self.using_meep = False
-        self.criterion = self.get_criterion(criterion, meep_start, meep_lambda)
+        self.criterion = self.get_criterion(criterion, reg_start, reg_lambda)
         self.optimizer_class = optimizer_class
         self.weight_decay = weight_decay
         self.lambda_lr = lambda_lr
@@ -123,20 +123,27 @@ class WMHModel(pl.LightningModule):
         self.output_dir = kwargs.get('output_dir', None)
         self.saved_preds = []
         self.patch_size = kwargs.get('patch_size', None)
+        self.mc_dropout_ratio = kwargs.get('mc_dropout_ratio', 0.0)
+        self.mc_dropout_samples = kwargs.get('mc_dropout_samples', 0)
 
         self.save_hyperparameters(ignore=['net'])
 
     @classmethod
-    def load_test(cls, model_path, save_predictions, output_dir, patch_size):
+    def load_test(cls, model_path, save_predictions, output_dir, patch_size,
+                  mc_dropout_ratio, mc_dropout_samples):
         model_path = os.path.expanduser(model_path)
-        obj = WMHModel.load_from_checkpoint(model_path, net=UNet3D())
+        obj = WMHModel.load_from_checkpoint(model_path,
+                                            net=UNet3D(mc_dropout_ratio))
         obj.model_path = os.path.abspath(model_path)
         obj.save_preds = save_predictions
         obj.output_dir = output_dir
         obj.patch_size = patch_size
+        obj.mc_dropout_ratio = mc_dropout_ratio
+        obj.mc_dropout_samples = mc_dropout_samples
+
         return obj
 
-    def get_criterion(self, loss, meep_start=0, meep_lambda=0.3):
+    def get_criterion(self, loss, meep_start=0, reg_lambda=0.3):
         match loss.lower():
             case 'crossentropy' | 'ce':
                 return CrossEntropyLoss()
@@ -148,7 +155,10 @@ class WMHModel(pl.LightningModule):
                 return FocalLoss()
             case 'cemeep' | 'crosentropymeep' | 'meep':
                 self.using_meep = True
-                return BCEMEEPLoss(meep_start, meep_lambda)
+                return BCEMEEPLoss(meep_start, reg_lambda)
+            case 'cekl' | 'crosentropykl' | 'kl':
+                self.using_meep = True
+                return BCEKLLoss(meep_start, reg_lambda)
             case _:
                 raise ValueError(f'Unknown loss function: {loss}')
 
@@ -175,7 +185,8 @@ class WMHModel(pl.LightningModule):
         os.makedirs(pred_folder, exist_ok=True)
         return pred_folder
 
-    def save_preds_tst(self, y_hat, y, logits, reference_imgs, is_test=False):
+    def save_preds_tst(self, y_hat, y, logits, reference_imgs, is_test=False,
+                       lgs_mc_arr=None, sm_mc_arr=None):
         """ Saves predictions to the disk
 
         It expects full volume images, not patches.
@@ -185,6 +196,8 @@ class WMHModel(pl.LightningModule):
         :param logits: Logits
         :param reference_imgs: Paths to the T1 images
         :param is_test: Whether the model is being tested or not
+        :param lgs_mc_arr: Logits array (for MC dropout)
+        :param sm_mc_arr: Softmax output array (for MC dropout)
         """
         if not is_test or (not self.save_preds and self.output_dir is None):
             return None
@@ -193,6 +206,17 @@ class WMHModel(pl.LightningModule):
             pred_folder = self.get_pred_folder(t1_path)
             hard_pred = torch.argmax(y_hat[i], dim=0).to(torch.uint8)
 
+            lgs_mc_mean = lgs_mc_arr.mean(0) if lgs_mc_arr is not None else None
+            sftmx_mc_mean = sm_mc_arr.mean(0) if sm_mc_arr is not None else None
+            hard_pred_mc, uncert_mc = None, None
+            if sm_mc_arr is not None:
+                hard_pred_mc = torch.argmax(sftmx_mc_mean[0], 0).to(torch.uint8)
+                if self.mc_dropout_samples == 1:
+                    print('WARNING: MC dropout samples == 1, uncertainty '
+                          'estimation won\'t be computed')
+                uncert_mc = sm_mc_arr.std(0)[0] if self.mc_dropout_samples > 1 \
+                    else None  # First elem in batch + foreground ch on save
+
             run_id = os.path.splitext(os.path.basename(self.model_path))[0]
             imgs = {
                 f'pred_wmh_hard_{run_id}.nii.gz': hard_pred,
@@ -200,6 +224,10 @@ class WMHModel(pl.LightningModule):
                 f'pred_logits_{run_id}.nii.gz': logits[i],
                 f'gt_wmh_{run_id}.nii.gz':
                     y[i, 1].to(torch.uint8) if y is not None else None,
+                f'pred_mc_logitsmean_{run_id}.nii.gz': lgs_mc_mean[0],
+                f'pred_mc_softmaxmean_{run_id}.nii.gz': sftmx_mc_mean[0],
+                f'pred_mc_hardmean_{run_id}.nii.gz': hard_pred_mc,
+                f'pred_mc_uncertmc_{run_id}.nii.gz': uncert_mc[1],
             }
 
             paths = []
@@ -215,43 +243,47 @@ class WMHModel(pl.LightningModule):
         if not is_test or self.patch_size is None:
             return self.net(x)
         else:
-            out_tensor = torch.empty((0, x.shape[1], x.shape[2], x.shape[3],
-                                      x.shape[4]))
-            for i_subj in range(x.shape[0]):
-                if 'wmh' in batch:
-                    subject = tio.Subject(
-                        t1=tio.ScalarImage(tensor=batch['t1']['data'][i_subj]),
-                        flair=tio.ScalarImage(
-                            tensor=batch['flair']['data'][i_subj]
-                        ),
-                        wmh=tio.LabelMap(tensor=batch['wmh']['data'][i_subj]))
-                grid_sampler = tio.inference.GridSampler(
-                    subject,
-                    self.patch_size,
-                    4,
-                )
-                patch_loader = torch.utils.data.DataLoader(
-                    grid_sampler,
-                    batch_size=1,
-                )
-                aggregator = tio.inference.GridAggregator(grid_sampler)
-                with torch.no_grad():
-                    for patches_batch in patch_loader:
-                        xc1 = patches_batch['t1'][tio.DATA]
-                        xc2 = patches_batch['flair'][tio.DATA]
+            return self.patch_inference(x, batch)
 
-                        # Concatenate the input images along the channel dimension
-                        x = torch.cat((xc1, xc2), dim=1)
+    def patch_inference(self, x, batch):
+        out_tensor = torch.empty((0, x.shape[1], x.shape[2], x.shape[3],
+                                  x.shape[4]))
+        for i_subj in range(x.shape[0]):
+            if 'wmh' in batch:
+                subject = tio.Subject(
+                    t1=tio.ScalarImage(tensor=batch['t1']['data'][i_subj]),
+                    flair=tio.ScalarImage(
+                        tensor=batch['flair']['data'][i_subj]
+                    ),
+                    wmh=tio.LabelMap(tensor=batch['wmh']['data'][i_subj]))
+            grid_sampler = tio.inference.GridSampler(
+                subject,
+                self.patch_size,
+                4,
+            )
+            patch_loader = torch.utils.data.DataLoader(
+                grid_sampler,
+                batch_size=1,
+            )
+            aggregator = tio.inference.GridAggregator(grid_sampler)
+            with torch.no_grad():
+                for patches_batch in patch_loader:
+                    xc1 = patches_batch['t1'][tio.DATA]
+                    xc2 = patches_batch['flair'][tio.DATA]
 
-                        locations = patches_batch[tio.LOCATION]
-                        logits = self.net(x)
-                        aggregator.add_batch(logits, locations)
-                output_tensor = aggregator.get_output_tensor()
-                # Append it to the out_tensor in the first channel (batch)
-                # out_tensor is a 5D tensor (batch, channels, x, y, z)
-                # and output_tensor is a 4D tensor (channels, x, y, z)
-                out_tensor = torch.cat((out_tensor, output_tensor.unsqueeze(0)))
-            return out_tensor
+                    # Concatenate the input images along the channel dimension
+                    x = torch.cat((xc1, xc2), dim=1)
+
+                    locations = patches_batch[tio.LOCATION]
+
+                    logits = self.net(x)
+                    aggregator.add_batch(logits, locations)
+            output_tensor = aggregator.get_output_tensor()
+            # Append it to the out_tensor in the first channel (batch)
+            # out_tensor is a 5D tensor (batch, channels, x, y, z)
+            # and output_tensor is a 4D tensor (channels, x, y, z)
+            out_tensor = torch.cat((out_tensor, output_tensor.unsqueeze(0)))
+        return out_tensor
 
     def infer_batch(self, batch, is_test=False):
         xc1 = batch['t1'][tio.DATA]
@@ -262,20 +294,61 @@ class WMHModel(pl.LightningModule):
         # Concatenate the input images along the channel dimension
         x = torch.cat((xc1, xc2), dim=1)
 
-        # Forward pass through the neural network
+        # Get MC dropout predictions if it applies (logits and softmax)
+        lgs_mc_arr, sm_mc_arr = self.get_mc_preds(x, batch, is_test)
+
+        # Forward pass
         logits = self.forward_pass(x, batch, is_test)
         y_hat = F.softmax(logits, dim=1)
 
-        self.save_preds_tst(y_hat, y, logits, t1_paths, is_test)
+        # Save predictions if it applies
+        self.save_preds_tst(y_hat, y, logits, t1_paths, is_test, lgs_mc_arr,
+                            sm_mc_arr)
+        return y_hat, y  # Return the predicted and GT masks (for train)
 
-        return y_hat, y  # Return the predicted and GT masks
+    def get_mc_preds(self, x, batch, is_test):
+        # For MC dropout:
+        mc_cond = self.mc_dropout_ratio > 0 and is_test
+        if mc_cond:
+            model_was_eval = not self.net.training
+            self.net.train()
+
+        # If no MC dropout -> return empty arrays
+        mc_fwd_passes = self.mc_dropout_samples if mc_cond else 0
+        logits_arr = torch.empty((mc_fwd_passes, x.shape[0], 2, x.shape[2],
+                                  x.shape[3], x.shape[4]))
+        y_hat_arr = torch.empty((mc_fwd_passes, x.shape[0], 2, x.shape[2],
+                                 x.shape[3], x.shape[4]))
+
+        # MC forward passes
+        for i in range(mc_fwd_passes):
+            print(f'  - MC dropout: forward pass {i + 1}/{mc_fwd_passes}')
+            logits_mc = self.forward_pass(x, batch, is_test)
+            y_hat_mc = F.softmax(logits_mc, dim=1)
+            logits_arr[i] = logits_mc
+            y_hat_arr[i] = y_hat_mc
+
+        # Restore the model to its original state
+        if mc_cond and model_was_eval:
+            self.net.eval()
+
+        return logits_arr, y_hat_arr
 
     def training_step(self, batch, batch_idx):
         epoch = self.current_epoch
         y_hat, y = self.infer_batch(batch)
-        loss = self.criterion(y_hat, y, epoch) if self.using_meep \
+        losses = self.criterion(y_hat, y, epoch) if self.using_meep \
             else self.criterion(y_hat, y)
         metrics = compute_metrics(y_hat, y, text='train_')
+
+        loss = 0
+        if type(losses) == dict:
+            for key in losses:
+                self.log(f'train_{key}', losses[key], prog_bar=True,
+                         on_step=True, on_epoch=True)
+                loss += losses[key]
+        else:
+            loss = losses
 
         self.log('train_loss', loss, prog_bar=True, on_step=True, on_epoch=True)
         self.log_dict(metrics, on_step=True, on_epoch=True)
@@ -285,9 +358,18 @@ class WMHModel(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         epoch = self.current_epoch
         y_hat, y = self.infer_batch(batch)
-        loss = self.criterion(y_hat, y, epoch) if self.using_meep \
+        losses = self.criterion(y_hat, y, epoch) if self.using_meep \
             else self.criterion(y_hat, y)
         metrics = compute_metrics(y_hat, y, text='val_')
+
+        loss = 0
+        if type(losses) == dict:
+            for key in losses:
+                self.log(f'val_{key}', losses[key], prog_bar=True, on_step=True,
+                         on_epoch=True)
+                loss += losses[key]
+        else:
+            loss = losses
 
         self.log('val_loss', loss, on_step=True, on_epoch=True)
         self.log_dict(metrics, on_step=True, on_epoch=True)

src/plot.py
@@ -21,11 +21,20 @@ def get_array_from_nifti(path):
     return sitk.GetArrayFromImage(sitk.ReadImage(path))
 
 
-def dice_scores(base_center, path_csvs=None):
+def compare_runs(runs_to_compare, metric_fn, **kwargs):
+    for run_name, run_path in runs_to_compare.items():
+        # print(f"Generating plot for {run_name}...")
+        metric_fn(run_path, run_name, **kwargs)
+        print('\n')
+
+
+def dice_scores(path_csvs=None, alt_title=''):
+    old_dir = os.getcwd() if path_csvs else None
+    path_csvs = os.path.abspath(path_csvs) if path_csvs else None
     os.chdir(path_csvs) if path_csvs else None
     dc = {}
     for center in centers:
-        df = pd.read_csv(f"{base_center}-{center}.csv", header=None)
+        df = pd.read_csv(f"{center}.csv", header=None)
         df.columns = csv_cols
         dc[center] = {'hard': []}
 
@@ -49,11 +58,12 @@ def dice_scores(base_center, path_csvs=None):
         axs[i].set_title(f"{center}")
         axs[i].set_ylim([0, 1])
 
-    fig.suptitle('Dice Scores for WMH Segmentation')
+    fig.suptitle((f'Dice Scores ({alt_title})' if alt_title else 'Dice Scores'))
     fig.text(0.5, 0.04, 'Center', ha='center')
     fig.text(0.04, 0.5, 'Dice Score', va='center', rotation='vertical')
 
     fig.subplots_adjust(top=0.85, wspace=0.05)
+    os.chdir(old_dir) if old_dir else None
 
 
 def entropy(probs, eps=1e-3, apply_mean=True):
@@ -142,18 +152,22 @@ def entropy_segment(base_center, path_csvs=None):
         axs[i].boxplot(values_gt, positions=[1], widths=0.6)
         axs[i].boxplot(values_bMask, positions=[2], widths=0.6)
 
+        # axs[i].set_xticks(range(3))
         axs[i].set_xticklabels(['Softmax > 0.5', 'Softmax in GT==1',
                                 'Softmax in brain mask'])
         axs[i].set_title(f"{center}")
 
 
-def entropy_segment_per_center(base_center, path_csvs=None):
+def entropy_segment_per_center(path_csvs=None, alt_title=None):
+    old_dir = os.getcwd() if path_csvs else None
+    path_csvs = os.path.abspath(path_csvs) if path_csvs else None
     os.chdir(path_csvs) if path_csvs else None
+
     segment_types = ['pred', 'gt', 'bMask']
     ent = {center_type: {} for center_type in segment_types}
 
     for center in centers:
-        df = pd.read_csv(f"{base_center}-{center}.csv", header=None)
+        df = pd.read_csv(f"{center}.csv", header=None)
         df.columns = csv_cols
 
         for segment_type in segment_types:
@@ -188,21 +202,26 @@ def entropy_segment_per_center(base_center, path_csvs=None):
             ent['gt'][center][subj] = entropy_gt_1
             ent['bMask'][center][subj] = entropy_brain
 
-    # Print the entropy for each center and individual
-    for segment_type in segment_types:
-        print(f"Segment type: {segment_type}")
-        for key, value in ent[segment_type].items():
-            print(f"{key}: {value}")
+    # # Print the entropy for each center and individual
+    # for segment_type in segment_types:
+    #     print(f"Segment type: {segment_type}")
+    #     for key, value in ent[segment_type].items():
+    #         print(f"{key}: {value}")
 
     fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(12, 8))
     for i, segment_type in enumerate(segment_types):
         for j, center in enumerate(centers):
             values = list(ent[segment_type][center].values())
             axs[i].boxplot(values, positions=[j], widths=0.6)
+
+        # axs[i].set_xticks(range(len(centers)))
         axs[i].set_xticklabels(centers)
         axs[i].set_title(f"{segment_type}")
 
+    if alt_title:
+        plt.suptitle(alt_title)
     plt.show()
+    os.chdir(old_dir) if old_dir else None
 
 
 def append_round(imgs, img, decimals=2):
@@ -360,10 +379,11 @@ def ece_reliability_sym(base_center, path_csvs=None):
     plt.show()
 
 
-def ece_reliability(base_center, path_csvs=None):
-    os.chdir(path_csvs) if path_csvs else None
+def ece_reliability(path_csvs=None, alt_title=None):
     for center in centers:
-        df = pd.read_csv(f"{base_center}-{center}.csv", header=None)
+        center_path = os.path.join(os.path.abspath(path_csvs),
+                                  f"{center}.csv")
+        df = pd.read_csv(center_path, header=None)
         df.columns = csv_cols
 
         preds_1 = np.array([])
@@ -400,10 +420,12 @@ def ece_reliability(base_center, path_csvs=None):
         ax.legend()
         ax.set_title(f"{center} - Reliability diagram\nECE: {ece_0:.4f}")
 
+        if alt_title:
+            fig.suptitle(alt_title)
     plt.show()
 
 
-def dice_vs_entropy(base_center, path_csvs=None):
+def dice_vs_entropy(path_csvs=None, alt_title=None, mask='brain'):
     """ Dice vs entropy plot
 
     Compute for each subject the dice score and the entropy of the softmax, and
@@ -413,12 +435,12 @@ def dice_vs_entropy(base_center, path_csvs=None):
 
     The y axis limits are 0-1 and the x axis its 0 to 0.2 more than the maximum
 
-    :param base_center:
-    :return:
     """
-    os.chdir(path_csvs) if path_csvs else None
+    plt.figure()
     for center in centers:
-        df = pd.read_csv(f"{base_center}-{center}.csv", header=None)
+        center_path = os.path.join(os.path.abspath(path_csvs),
+                                  f"{center}.csv")
+        df = pd.read_csv(center_path, header=None)
         df.columns = csv_cols
 
         dice_ent = {center: []}
@@ -429,20 +451,25 @@ def dice_vs_entropy(base_center, path_csvs=None):
 
             pred_hard = nib.load(pred_hard_path).get_fdata()
             gt = nib.load(gt_path).get_fdata()
-            b_mask = nib.load(get_b_mask_path(subj_path)).get_fdata()
-            b_mask = b_mask.flatten()
+
+            pred_softmax = nib.load(pred_softmax_path).get_fdata()
+            pos_class = pred_softmax[:, :, :, 1].flatten()
+
+            # Load the mask
+            if mask == 'brain':
+                p_mask = nib.load(get_b_mask_path(subj_path)).get_fdata()
+                p_mask = p_mask.flatten()
+            elif mask == 'gt':
+                p_mask = gt.flatten()
+            elif mask == 'softmax':
+                p_mask = np.where(pos_class > 0.5, 1, 0)
 
             # Compute dice scores
             dice_hard = dice_score(pred_hard, gt)
 
-            # Filtrar por GT == 1
-
             # Compute entropy
-            pred_softmax = nib.load(pred_softmax_path).get_fdata()
-            pos_class = pred_softmax[:, :, :, 1].flatten()
-            # thres_pos = np.where(pos_class >= 0.5)[0]
-            thres_brain = np.where(b_mask == 1)[0]
-            filt_softmax = pos_class[thres_brain]
+            thres_mask = np.where(p_mask == 1)[0]
+            filt_softmax = pos_class[thres_mask]
 
             ent = entropy(filt_softmax)
 
@@ -460,22 +487,26 @@ def dice_vs_entropy(base_center, path_csvs=None):
     plt.ylim([0, 1])
 
     plt.legend()
+    if alt_title:
+        plt.title(alt_title)
+
     plt.show()
 
 
-def uncertainty_by_condition(base_center, path_csvs=None):
+def uncertainty_by_condition(path_csvs=None, alt_title='', n_samples=None):
     """ Uncertainty by condition
 
     Plots inspired in: uncertainty_by_condition from https://github.com/SteffenCzolbe/probabilistic_segmentation  # noqa
     This will plot the TP, FP, TN, FN uncertainty for each center
 
-    :param base_center: Base center
     :return:
     """
+    old_dir = os.getcwd() if path_csvs else None
+    path_csvs = os.path.abspath(path_csvs) if path_csvs else None
     os.chdir(path_csvs) if path_csvs else None
     unc_cond = {}
     for center in centers:
-        df = pd.read_csv(f"{base_center}-{center}.csv", header=None)
+        df = pd.read_csv(f"{center}.csv", header=None)
         df.columns = csv_cols
 
         unc_cond[center] = {
@@ -507,7 +538,7 @@ def uncertainty_by_condition(base_center, path_csvs=None):
         thres_brain = np.where(b_mask == 1)[0]
         pos_brain = pos_sftmx[thres_brain]
         neg_brain = neg_sftmx[thres_brain]
-        pos_gt_brain = pos_gt[thres_brain]  # Not necessary but just in case
+        pos_gt_brain = pos_gt[thres_brain]
         neg_gt_brain = neg_gt[thres_brain]
 
         # TP
@@ -530,29 +561,43 @@ def uncertainty_by_condition(base_center, path_csvs=None):
         fn_unc = entropy(neg_brain[fn], apply_mean=False)
         unc_cond[center]["FN"] = np.append(unc_cond[center]["FN"], fn_unc)
 
+        # Since there are too many points on TN, we will sample all categories
+        # for having just 100 samples
+        if n_samples:
+            for category in unc_cond[center].keys():
+                if len(unc_cond[center][category]) > n_samples:
+                    unc_cond[center][category] = np.random.choice(
+                        unc_cond[center][category], n_samples, replace=False)
+
     # Plot Stripplot
     fig, ax = plt.subplots(len(centers), 1, figsize=(4, 10))
     categories = ['TP', 'FP', 'TN', 'FN']
     for i, center in enumerate(centers):
-        sns.stripplot(data=unc_cond[center], ax=ax[i], jitter=True, alpha=0.03)
+        sns.stripplot(data=unc_cond[center], ax=ax[i], jitter=True, alpha=0.05)
         ax[i].set_title(center)
         ax[i].set_ylabel("Entropy")
-        ax[i].set_xticks([0, 1, 2, 3])
+
+        ax[i].set_xticks(range(len(categories)))
         ax[i].set_xticklabels(categories)
 
         # Calculate mean and median for each category
-        mean_values = [np.mean(unc_cond[center][category]) for category in categories]
-        median_values = [np.median(unc_cond[center][category]) for category in categories]
+        mean_values = [np.mean(unc_cond[center][category]) for category in
+                       categories]
+        median_values = [np.median(unc_cond[center][category]) for category in
+                         categories]
 
         # Add mean and median markers for each category
         for j, category in enumerate(categories):
-            ax[i].plot(j, mean_values[j], marker='o', color='red', label='Mean' if j == 0 else '', zorder=10)
-            ax[i].plot(j, median_values[j], marker='o', color='blue', label='Median' if j == 0 else '', zorder=10)
+            ax[i].plot(j, mean_values[j], marker='o', color='red',
+                       label='Mean' if j == 0 else '', zorder=10)
+            ax[i].plot(j, median_values[j], marker='o', color='blue',
+                       label='Median' if j == 0 else '', zorder=10)
 
         # Add legend
         ax[i].legend()
 
+    if alt_title:
+        fig.suptitle(alt_title)
 
     plt.show()
-
-
+    os.chdir(old_dir) if old_dir else None